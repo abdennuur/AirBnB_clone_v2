d02530ffa1a1436b36fd9e62dce72daed738c1e4
abdennuur <abdnourkhatib@gmail.com>
2024-03-18 09:23:01 -0700

    review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""define Review class"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Represent review for MySQL database.
+
+    Inherits frm SQLAlchemy Base and links to MySQL table reviews.
+
+    Attributes:
+        __tablename__ (str): name of the MySQL table to store Reviews.
+        text (sqlalchemy String): review description.
+        place_id (sqlalchemy String): review's place id.
+        user_id (sqlalchemy String): review's user id.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
