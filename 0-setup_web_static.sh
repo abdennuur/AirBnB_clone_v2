29335b3914309ab364765bd4f97b50b5f8329d23
widad <widad.moumkine@gmail.com>
2024-04-04 22:11:58 +0400

    made files executable & succeeded

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
-# Setup web server for deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+# Script to set up web servers for deployment of web_static
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Install Nginx if not already installed
+if ! dpkg -l nginx &> /dev/null; then
+    apt-get -y update
+    apt-get -y install nginx
+fi
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create necessary directories if they don't exist
+mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
-    location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
-    }
+# Create symbolic link, remove if already exists
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 
-    location /redirect_me {
-	return 301 http://cuberule.com/;
-    }
+# Set ownership of /data/ to ubuntu user and group
+chown -R ubuntu:ubuntu /data/
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# Update Nginx configuration
+config="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+sed -i "/server_name _;/a $config" /etc/nginx/sites-available/default
 
+# Restart Nginx
 service nginx restart
+
+exit 0
