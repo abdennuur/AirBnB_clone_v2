a473726057ce9e76e9f657eff911d31747caa1d2
abdnennuur <abdnourkhatib@gmail.com>
2024-04-18 03:51:56 -0700

    2 tasks

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+To start Flask web app
+"""
+
+from flask import Flask
+
+my_App = Flask(__name__)
+
+
+@my_App.route('/', strict_slashes=False)
+def hello_HBNB():
+    """displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@my_App.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """display HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    my_App.run(host='0.0.0.0', port=5000)
