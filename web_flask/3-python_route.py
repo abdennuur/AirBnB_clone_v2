d2c443bca98096abb0f9523954742b9fca4007b7
abdennuur <abdnourkhatib@gmail.com>
2024-04-19 02:36:17 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -32,5 +32,6 @@ def display_P(text="is cool"):
     (replace underscore _ symbols with a space )"""
     return 'Python ' + text.replace('_', ' ')
 
+
 if __name__ == '__main__':
     my_App.run(host='0.0.0.0', port='5000')
