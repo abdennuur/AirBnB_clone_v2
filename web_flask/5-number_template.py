77de1903f7b425fcd0987d56c788e2524be89140
abdennuur <abdnourkhatib@gmail.com>
2024-04-19 06:54:44 -0700

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Flask web app
+"""
+
+from flask import Flask, render_template
+my_App = Flask(__name__)
+
+
+@my_App.route('/', strict_slashes=False)
+def d_HELLO_HBNB():
+    """display “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+
+@my_App.route('/hbnb', strict_slashes=False)
+def d_HBNB():
+    """display “HBNB”"""
+    return 'HBNB'
+
+
+@my_App.route('/c/<text>', strict_slashes=False)
+def d_C(text):
+    """display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@my_App.route('/python', strict_slashes=False)
+@my_App.route('/python/<text>', strict_slashes=False)
+def d_P(text='is cool'):
+    """display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@my_App.route('/number/<int:n>', strict_slashes=False)
+def isnbr(nbr):
+    """display “n is a nbr” only if n is int"""
+    return "{:d} is a number".format(nbr)
+
+
+@my_App.route('/number_template/<int:n>', strict_slashes=False)
+def template_Nbr(nbr):
+    """display HTML page only if n is int"""
+    return render_template('5-number.html', nbr=nbr)
+
+
+if __name__ == '__main__':
+    my_App.run(host='0.0.0.0', port='5000')
