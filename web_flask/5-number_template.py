0e7693941de136e649d20248749e4800ebad60aa
abdennuur <abdnourkhatib@gmail.com>
2024-04-19 07:44:35 -0700

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,40 +9,40 @@ my_App = Flask(__name__)
 @my_App.route('/', strict_slashes=False)
 def d_HELLO_HBNB():
     """display “Hello HBNB!”"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @my_App.route('/hbnb', strict_slashes=False)
 def d_HBNB():
     """display “HBNB”"""
-    return 'HBNB'
+    return "HBNB"
 
 
 @my_App.route('/c/<text>', strict_slashes=False)
 def d_C(text):
     """display “C ”, followed by the value of the text variable
     (replace underscore _ symbols with a space )"""
-    return 'C ' + text.replace('_', ' ')
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@my_App.route('/python', strict_slashes=False)
+@my_App.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @my_App.route('/python/<text>', strict_slashes=False)
-def d_P(text='is cool'):
+def d_P(text):
     """display “Python ”, followed by the value of the text variable
     (replace underscore _ symbols with a space )"""
-    return 'Python ' + text.replace('_', ' ')
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 @my_App.route('/number/<int:n>', strict_slashes=False)
-def isnbr(nbr):
+def isnbr(n):
     """display “n is a nbr” only if n is int"""
-    return "{:d} is a number".format(nbr)
+    return "{} is a number".format(n)
 
 
 @my_App.route('/number_template/<int:n>', strict_slashes=False)
-def template_Nbr(nbr):
+def template_Nbr(n):
     """display HTML page only if n is int"""
-    return render_template('5-number.html', nbr=nbr)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
