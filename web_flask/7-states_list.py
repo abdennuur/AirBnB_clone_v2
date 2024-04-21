a7313d3200674c4f22117b00f98ae68daa0648e0
abdennuur <abdnourkhatib@gmail.com>
2024-04-21 04:11:10 -0700

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,20 +5,20 @@
 from flask import Flask, render_template
 from models import *
 from models import storage
-app = Flask(__name__)
+my_App = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@my_App.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of all State objects"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
+@my_App.teardown_appcontext
 def teardown_db(exception):
-    """To Remove current SQLAlchemy Session"""
+    """To remove current SQLAlchemy Session"""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    my_App.run(host='0.0.0.0', port='5000')
